b0be94c84b6f4cc82ef2d6df3072c173985c1623
Photograph Korea <photographkorea@gmail.com>
2026-06-06 18:40:15 +0900

    fix: hero image aspect ratio and prevent cropping

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
       <img 
         src="/images/hero.jpg" 
         alt="Main Hero Image"
-        style={{ width: '100%', maxHeight: '400px', objectFit: 'cover' }}
+        style={{ width: '100%', height: 'auto' }}
         className="block"
       />
     </section>
